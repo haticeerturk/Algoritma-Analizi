feb1dbe9fa3eb76cc7751d5c1017c1e9958fa22e
Hatice ERTÜRK <haticeerturk27@gmail.com>
2017-02-24 13:19:40 +0200

    Add files via upload
    Function  a^b's result.

---
+++ week 1/power/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int power(int a, int b){
+    int toplam = 1;
+    for(int i=0; i<b; i++){
+        toplam *= a;
+    }
+    return toplam;
+}
+
+int main()
+{
+    int sonuc = power(3,4);
+    cout << sonuc;
+    return 0;
+}
